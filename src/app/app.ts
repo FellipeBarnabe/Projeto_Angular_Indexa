b07de50d4754e213497672f0d3d74f45d23f3214
Fellipe Barnabe <fellipebarnabe.stack@gmail.com>
2025-10-09 09:37:24 -0300

    primeiro commit indexa

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
+import { Container } from './componentes/container/container';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, Container],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('indexa');
+  title = 'indexa';
 }
