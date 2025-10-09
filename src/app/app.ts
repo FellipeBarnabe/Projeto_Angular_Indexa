f56d705eb5dec89b1a8faaf1a61f8e68cba3949f
Fellipe Barnabe <fellipebarnabe.stack@gmail.com>
2025-10-09 19:37:02 -0300

    header component

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import { Container } from './componentes/container/container';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './componentes/header/header';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, Container],
+  imports: [CommonModule, RouterOutlet, Container, Header],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
