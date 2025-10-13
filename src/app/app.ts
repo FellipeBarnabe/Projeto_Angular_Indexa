4202720af3e0d1a961cc8cfe444fe85b36915f23
Fellipe Barnabe <fellipebarnabe.stack@gmail.com>
2025-10-13 15:28:46 -0300

    adicionando Funcionalidade @for

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,14 +3,15 @@ import { Container } from './componentes/container/container';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './componentes/header/header';
+import { Separator } from './componentes/separator/separator';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, Container, Header],
+  imports: [CommonModule, RouterOutlet, Container, Header, Separator],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
-  title = 'indexa';
+  readonly alfabeto: string = ' A B C D E F G H I J K L M N O P Q R S T U V W X Y Z';
 }
