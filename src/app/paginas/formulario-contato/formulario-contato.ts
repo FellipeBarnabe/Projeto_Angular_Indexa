2dad5628b2b97413787e135370bceb31f254c4d8
Fellipe Barnabe <fellipebarnabe.stack@gmail.com>
2025-11-07 10:53:43 -0300

    feat/form/constructor/

--- src/app/paginas/formulario-contato/formulario-contato.ts
+++ src/app/paginas/formulario-contato/formulario-contato.ts
@@ -2,11 +2,30 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Container } from '../../componentes/container/container';
 import { Separator } from '../../componentes/separator/separator';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-contato',
-  imports: [CommonModule, Container, Separator],
+  imports: [CommonModule, Container, Separator, ReactiveFormsModule],
   templateUrl: './formulario-contato.html',
   styleUrl: './formulario-contato.css',
 })
-export class FormularioContato {}
+export class FormularioContato {
+  contatoForm!: FormGroup;
+  constructor() {
+    this.contatoForm = new FormGroup({
+      nome: new FormControl('fe'),
+      telefone: new FormControl('23345'),
+      email: new FormControl('asddads@gmail.com'),
+      aniversario: new FormControl('17/10/1989'),
+      redes: new FormControl('facebook'),
+      observacoes: new FormControl('Eu sou...'),
+    });
+  }
+  salvarContato() {
+    console.log(this.contatoForm.value);
+  }
+  cancelar() {
+    console.log('submissão cancelada');
+  }
+}
